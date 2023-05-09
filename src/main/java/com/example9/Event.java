c2805d4cc24ca595daebd2099148d0801ca636db
Jeremy Feng <luckyjiefeng@163.com>
2023-05-09 18:14:42 +0800

    EventBus

---
+++ src/main/java/com/example9/Event.java
@@ -0,0 +1,23 @@
+package com.example9;
+
+/**
+ * @author Feng Jie
+ * @date 2023/5/9 17:43
+ */
+public class Event {
+    private String type;
+    private String message;
+
+    public Event(String type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
---
+++ src/main/java/com/example9/Main.java
@@ -0,0 +1,22 @@
+package com.example9;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+/**
+ * @author Feng Jie
+ * @date 2023/5/9 17:53
+ */
+public class Main {
+    public static void main(String[] args) {
+        ActorSystem sys = ActorSystem.create("sys");
+        EventBusDemo eventBusDemo = new EventBusDemo();
+        ActorRef actor = sys.actorOf(Props.create(Subscriber.class), "suber");
+        eventBusDemo.subscribe(actor, "info");
+        eventBusDemo.subscribe(actor, "warn");
+        eventBusDemo.publish(new Event("info", "info level message"));
+        eventBusDemo.publish(new Event("other", "other level message"));
+
+    }
+}
