7038328889bbdaf3c97cbad1dc848701824c0970
Jeremy Feng <luckyjiefeng@163.com>
2023-05-09 11:16:33 +0800

    stop actor

---
+++ src/main/java/com/example5/WatchActor.java
@@ -0,0 +1,41 @@
+package com.example5;
+
+import akka.actor.ActorRef;
+import akka.actor.Props;
+import akka.actor.Terminated;
+import akka.actor.UntypedAbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.Optional;
+
+/**
+ * @author Feng Jie
+ * @date 2023/5/9 11:04
+ */
+public class WatchActor extends UntypedAbstractActor {
+    LoggingAdapter log = Logging.getLogger( this.getContext() .system(), this);
+    ActorRef child = null;
+    @Override
+    public void preStart() throws Exception {
+        child = getContext().actorOf(Props.create(WorkerActor.class), "workerActor");
+        getContext().watch(child);
+    }
+
+    @Override
+    public void postStop() throws Exception {
+        log.info ( "WatchActor posts top");
+    }
+
+    @Override
+    public void onReceive(Object message) throws Throwable {
+        if (message instanceof String){
+            if ("stop".equals(message)){
+                getContext().stop(child);
+            }
+        } else if (message instanceof Terminated) {
+            Terminated ter = (Terminated) message;
+            log.info("监控到：{} 停止了", ter.getActor());
+        }
+    }
+}
---
+++ src/main/java/com/example5/WorkerActor.java
@@ -0,0 +1,24 @@
+package com.example5;
+
+import akka.actor.UntypedAbstractActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+import scala.PartialFunction;
+import scala.runtime.BoxedUnit;
+
+/**
+ * @author Feng Jie
+ * @date 2023/5/9 11:06
+ */
+public class WorkerActor extends UntypedAbstractActor {
+    LoggingAdapter log = Logging.getLogger( this.getContext() .system(), this);
+    @Override
+    public void onReceive(Object message) throws Throwable {
+        log.info("收到消息：{}", message);
+    }
+
+    @Override
+    public void postStop() throws Exception {
+        log.info("worker 停止");
+    }
+}
