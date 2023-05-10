44a783f27e629e3c79eb9fe640d9defd4923755d
Jeremy Feng <luckyjiefeng@163.com>
2023-05-10 14:58:36 +0800

    TCP

---
+++ src/main/java/com/example11/Main.java
@@ -0,0 +1,16 @@
+package com.example11;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+
+/**
+ * @author Feng Jie
+ * @date 2023/5/10 11:32
+ */
+public class Main {
+    public static void main(String[] args) {
+        ActorSystem sys = ActorSystem.create("sys");
+        ActorRef tcpServer = sys.actorOf(Props.create(TcpServerDemo.class), "tcpServer");
+    }
+}
